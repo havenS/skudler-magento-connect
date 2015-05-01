55f6bd4933a8a0af5b9d6ae515588e43d6a8962c
julien <julien.fournier@inopale.fr>
2015-05-01 08:19:18 +0200

    Module Skeleton

---
+++ app/code/community/Skudler/Connect/Block/Adminhtml/Connectbackend.php
@@ -0,0 +1,5 @@
+<?php  
+
+class Skudler_Connect_Block_Adminhtml_Connectbackend extends Mage_Adminhtml_Block_Template {
+
+}
---
+++ app/code/community/Skudler/Connect/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Skudler_Connect_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
